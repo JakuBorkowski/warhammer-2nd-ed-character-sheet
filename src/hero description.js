8009db0ebedf2cb1de3e3bb78bbab4f153889019
Jakub Borkowski <jakub.bor2@gmail.com>
2022-01-23 20:06:34 +0100

    add data

--- src/hero description.js
+++ src/hero description.js
@@ -1,11 +1,31 @@
 function HeroDescriptioneron() {
-
-
+  const data = [
+    {name:"Wiek:", id:1,},
+    {name:"Płeć:", id:2,},
+    {name:"Kolor oczu:", id:3},
+    {name:"Waga:", id:4},
+    {name:"Kolor włosów:", id:5},
+    {name:"Wzrost:", id:6},
+    {name:"Znak gwiezdny:", id:7},
+    {name:"Rodzeństwo:", id:8},
+    {name:"Miejce urodzenia:", id:9,},
+    {name:"Znaki szczególne:", id:10},
+  ]
     return (
-      <>
-         <h1>OPIS BOCHATERA</h1>
+      <div className="element">
+         <h1 className="elementHeader">OPIS BOCHATERA</h1>
+         <ul className="listHeroDescriptioneron">
+          {data.map(data=>{return<li className={data.id<=8? "shortElement":"longElement"} key={data.id}>
+            <label className="form">
+              <p className="name">{data.name}</p>
+              <input className="heroInput"></input>
+              </label>
+            </li>
+          }
+          )}
+        </ul>
   
-      </>
+      </div>
     );
   }
   
--- src/hero.js
+++ src/hero.js
@@ -1,10 +1,27 @@
 function Hero() {
+  const elementID ="hero";
+  const data =[
+  {name:"Imię:", id:[elementID, 1]},
+  {name:"Rasa:", id:[elementID , 2]},
+  {name:"Obecna profesja:", id:[elementID , 3]},
+  {name:"Poprzednia profesja:", id:[elementID , 4]},
 
-
+]
   return (
     <>
-       <h1>BOCHATER</h1>
-
+    <div className="element" >
+       <h1 className="elementHeader">BOCHATER</h1>
+        <ul>
+          {data.map(data=>{return<li key={data.id}>
+            <label className="form">
+              <p className="name">{data.name}</p>
+              <input className="heroInput"></input>
+              </label>
+            </li>
+          }
+          )}
+        </ul>
+      </div>
     </>
   );
 }
