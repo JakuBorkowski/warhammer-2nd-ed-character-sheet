dfee7e3cc2faf989dbb73dbc668ca9f62d5cb9a6
Jakub <jakub.bor2@gmail.com>
2022-03-07 12:25:07 +0100

    beauty

--- src/elements/combat movment.js
+++ src/elements/combat movment.js
@@ -3,32 +3,35 @@ import LabelValueInfo from "./atoms/labelValueInfo";
 function CombatMovment() {
   const data = [
     {
-      id:1,
-      name:"Ruch/odwrót",
+      id: 1,
+      name: "Ruch/odwrót",
     },
     {
-      id:2,
-      name:"Szarża",
+      id: 2,
+      name: "Szarża",
     },
     {
-      id:3,
-      name:"Bieg",
+      id: 3,
+      name: "Bieg",
     },
-  ]
-
+  ];
 
   return (
-    <div className="element" >
-    <h1 className="elementHeader">PUNKTY DOŚWIADCZENIA</h1>
-     <ul className="combatMovmentUl">
-     {data.map(data=><LabelValueInfo
-       cName={data.id<2 ? "combatMovmentLi" :`combatMovmentLi borderLeft`}
-       key={data.id} 
-       name={data.name}/>)}
-     </ul>
-   </div>
-    );
-  }
-  
-  export default CombatMovment;
-  
+    <div className="element">
+      <h1 className="elementHeader">RUCH W WALCE</h1>
+      <ul className="combatMovmentUl">
+        {data.map((data) => (
+          <LabelValueInfo
+            cName={
+              data.id < 2 ? "combatMovmentLi" : `combatMovmentLi borderLeft`
+            }
+            key={data.id}
+            name={data.name}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CombatMovment;
